d43799a5a64ae49fe77664ed18fd5da7a24d9f22
TsykuraWork <bobrokrol1234@gmail.com>
2020-10-09 23:53:56 +0300

    setted the interval

--- weatherAPI/js/script.js
+++ weatherAPI/js/script.js
@@ -5,27 +5,22 @@ let temp = document.querySelectorAll('.tempreture');
 let weather = document.querySelectorAll('.weather-judge');
 let wind_speed = document.querySelectorAll('.wind-speed');
 let country = document.querySelectorAll('.country');
-console.log(country);
-
-let i;
-
-for(let i = 0; i < city_name.length; i++){
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${fetches[i]},ua&appid=fc1378d4e91f84ad3c6c0eed07909642`)
-.then(function (resp) {return resp.json() })
-.then(function (data) {
-    console.log(data);
-    city_name[i].innerHTML = data.name;
-    const temperature = `${(+data.main.temp - 273).toFixed(2)}°`;
-    temp[i].innerHTML = temperature;
-    weather[i].innerHTML = `Weather: ${data.weather[0].main}`;
-    wind_speed[i].innerHTML = `Wind speed: ${data.wind.speed}m/s`;
-    country[i].innerHTML = `Country: ${data.sys.country}`;
-})
-.catch(function () {
-
-})
-
-}
+let k;
+setInterval(() => {
+    for(let i = 0; i < city_name.length; i++){
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${fetches[i]},ua&appid=fc1378d4e91f84ad3c6c0eed07909642`)
+        .then(function (resp) {return resp.json() })
+        .then(function (data) {
+            city_name[i].innerHTML = data.name;
+            const temperature = `${(+data.main.temp - 273).toFixed(2)}°`;
+            temp[i].innerHTML = temperature;
+            weather[i].innerHTML = `Weather: ${data.weather[0].main}`;
+            wind_speed[i].innerHTML = `Wind speed: ${data.wind.speed}m/s`;
+            country[i].innerHTML = `Country: ${data.sys.country}`;
+        })
+        .catch(function () {})
+    }
+},2000)
 
 
 
