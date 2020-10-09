cb414128ac405163850c27a2cc4a0dc05205ca5f
TsykuraWork <bobrokrol1234@gmail.com>
2020-10-09 22:26:56 +0300

    added the fetch function

--- weatherAPI/js/script.js
+++ weatherAPI/js/script.js
@@ -1,1 +1,9 @@
 console.clear();
+fetch('https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=fc1378d4e91f84ad3c6c0eed07909642')
+.then(function (resp) {return resp.json() })
+.then(function (data) {
+    console.log(data);
+})
+.catch(function () {
+
+})
