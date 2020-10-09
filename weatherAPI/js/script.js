1da5766d64a60b5d7dfcbc017f3009b9bddd86a9
TsykuraWork <bobrokrol1234@gmail.com>
2020-10-10 01:20:20 +0300

    ended with timeout and finished app

--- weatherAPI/js/script.js
+++ weatherAPI/js/script.js
@@ -1,11 +1,26 @@
-console.clear();
+
+
+window.addEventListener('DOMContentLoaded', (event) => {
+
+
+        setTimeout(() => {
+        document.querySelector('.message').innerHTML+='.';
+    },1000);
+         setTimeout(() => {
+        document.querySelector('.message').innerHTML+='.';
+    },2000);
+         setTimeout(() => {
+        document.querySelector('.message').innerHTML+='.';
+    },2990);
+
+    
+
 const fetches = ['London,uk', 'Kyiv,ua', 'Donetsk,ua', 'Moscow,ru']
 let city_name = document.querySelectorAll('.city-name');
 let temp = document.querySelectorAll('.tempreture');
 let weather = document.querySelectorAll('.weather-judge');
 let wind_speed = document.querySelectorAll('.wind-speed');
 let country = document.querySelectorAll('.country');
-let k;
 setInterval(() => {
     for(let i = 0; i < city_name.length; i++){
         fetch(`https://api.openweathermap.org/data/2.5/weather?q=${fetches[i]},ua&appid=fc1378d4e91f84ad3c6c0eed07909642`)
@@ -20,8 +35,8 @@ setInterval(() => {
         })
         .catch(function () {})
     }
-},2000)
-
+},3000);
+});
 
 
 
