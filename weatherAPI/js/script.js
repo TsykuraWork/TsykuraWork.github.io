24c954aabab8a4620c0577d78a5ede178d8b3c7c
TsykuraWork <bobrokrol1234@gmail.com>
2020-10-09 23:46:54 +0300

    Cycled it and got 4 fetches!

--- weatherAPI/js/script.js
+++ weatherAPI/js/script.js
@@ -1,18 +1,32 @@
 console.clear();
-fetch('https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=fc1378d4e91f84ad3c6c0eed07909642')
+const fetches = ['London,uk', 'Kyiv,ua', 'Donetsk,ua', 'Moscow,ru']
+let city_name = document.querySelectorAll('.city-name');
+let temp = document.querySelectorAll('.tempreture');
+let weather = document.querySelectorAll('.weather-judge');
+let wind_speed = document.querySelectorAll('.wind-speed');
+let country = document.querySelectorAll('.country');
+console.log(country);
+
+let i;
+
+for(let i = 0; i < city_name.length; i++){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${fetches[i]},ua&appid=fc1378d4e91f84ad3c6c0eed07909642`)
 .then(function (resp) {return resp.json() })
 .then(function (data) {
     console.log(data);
-    document.querySelector('.city-name').innerHTML = data.name;
-    const temp = `${(+data.main.temp - 273).toFixed(2)}°`;
-    document.querySelector('.tempreture').innerHTML = temp;
-    document.querySelector('.weather-judge').innerHTML = `Weather: ${data.weather[0].main}`;
-    document.querySelector('.wind-speed').innerHTML = `Wind speed: ${data.wind.speed}m/s`;
-    document.querySelector('.country').innerHTML = `Country: ${data.sys.country}`;
+    city_name[i].innerHTML = data.name;
+    const temperature = `${(+data.main.temp - 273).toFixed(2)}°`;
+    temp[i].innerHTML = temperature;
+    weather[i].innerHTML = `Weather: ${data.weather[0].main}`;
+    wind_speed[i].innerHTML = `Wind speed: ${data.wind.speed}m/s`;
+    country[i].innerHTML = `Country: ${data.sys.country}`;
 })
 .catch(function () {
 
 })
+
+}
+
 
 
 
